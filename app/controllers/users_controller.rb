0ae9e309adf96878b224d80f29b13996bdbdfe03
Zhuang Xu <not-real@example.com>
2020-01-16 11:13:15 +1100

    Added the sign-up flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create user_params
+
+    if @user.persisted?
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def show
@@ -19,5 +29,10 @@ class UsersController < ApplicationController
 
   def map
     @users = User.select(:name, :latitude, :longitude) # all rows, certain columns
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
